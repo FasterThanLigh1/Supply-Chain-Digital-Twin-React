4d64d456f8587d9fa7882b4abc68838d5527176f
Quan U Minh <75129911+FasterThanLigh1@users.noreply.github.com>
2023-05-03 01:40:42 +0700

    Add landing page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Supply Chain Digital Twin
- 
---
+++ supply-chain-sim/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#66c2ff",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
